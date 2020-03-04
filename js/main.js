b393396216e0873fc5e91cc3040976cb856e9c7f
eilisglee <elee2@seattleu.edu>
2020-03-04 11:56:04 -0800

    images, js, css

--- js/main.js
+++ js/main.js
@@ -1,4 +1,39 @@
 
+let iconBx = document.querySelectorAll('.iconBx');
+let contentBx = document.querySelectorAll('.contentBx');
+
+for (var i = 0; i < iconBx.length; i++) {
+    iconBx[i].addEventListener('mouseover', function () {
+        for (var i = 0; i < contentBx.length; i++) {
+            contentBx[i].className = 'contentBx';
+        }
+        document.getElementById(this.dataset.id).className = 'contentBx active';
+
+        for (var i = 0; i < iconBx.length; i++) {
+            iconBx[i].className = 'iconBx';
+        }
+        this.className = 'iconBx active';
+    });
+}
+
+function clearForm(oForm) {
+    var elements = oForm.elements;
+    oForm.reset();
+    for (i = 0; i < elements.length; i++) {
+        field_type = elements[i].type.toLowerCase();
+        switch (field_type) {
+            case "text":
+            default:
+                break;
+        }
+    }
+}
+
+let processInput = document.getElementById("process-input");
+
+processInput.addEventListener("click", function (event) {
+    this.innerHTML = "Give it another go?";
+})
 
 document.addEventListener('DOMContentLoaded', (event) => {
     document.addEventListener('submit', (event) => {
@@ -22,21 +57,28 @@ document.addEventListener('DOMContentLoaded', (event) => {
         let occupations = [(occupation1), (occupation2), (occupation3)];
         let occupationValue = occupations[Math.floor(Math.random() * 3)];
 
-
-        let home = ['mansion', 'apartment', 'shack', 'house'];
+        let home = ['Mansion', 'Apartment', 'Shack', 'House'];
         let homeValue = home[Math.floor(Math.random() * 4)];
-
         let salary = ['$100', '$100,000', '$1,000,000', '$100,000,000'];
         let salaryValue = salary[Math.floor(Math.random() * 4)];
-
         let numOfKids = Math.floor(Math.random() * 10);
 
-        // let processInput = document.getElementById("process-input");
-        let displayOutput = document.getElementById("display-output");
+        let displayOutput1 = document.getElementById("display-output1");
+        let displayOutput2 = document.getElementById("display-output2");
+        let displayOutput3 = document.getElementById("display-output3");
+        let displayOutput4 = document.getElementById("display-output4");
+        let displayOutput5 = document.getElementById("display-output5");
+        let displayOutput6 = document.getElementById("display-output6");
 
-        displayOutput.innerHTML = "You will marry " + spouseValue.value + " and together you'll have " + numOfKids + " kids and live in a " + homeValue + ". You'll drive a " + carValue.value + " and work as a " + occupationValue.value + ", earning a salary of " + salaryValue + " per year.";
+        displayOutput1.innerHTML = homeValue;
+        displayOutput2.innerHTML = spouseValue.value;
+        displayOutput3.innerHTML = numOfKids;
+        displayOutput4.innerHTML = carValue.value;
+        displayOutput5.innerHTML = occupationValue.value;
+        displayOutput6.innerHTML = salaryValue;
     });
 });
+
 
 
 
